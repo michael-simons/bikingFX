b44e17d75fd4ba712e59821ad02d65489e22dd39
Michael J. Simons <michael@simons.ac>
2014-10-16 18:00:42 +0200

    Replace static tests with bikingpictures from api

--- src/main/java/ac/simons/bikingFX/FXMLController.java
+++ src/main/java/ac/simons/bikingFX/FXMLController.java
@@ -1,13 +1,19 @@
 package ac.simons.bikingFX;
 
+import ac.simons.bikingFX.bikingPictures.BikingPicture;
+import ac.simons.bikingFX.bikingPictures.BikingPictureRetrievalService;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.util.stream.Collectors;
+import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 
 public class FXMLController implements Initializable {
@@ -18,13 +24,16 @@ public class FXMLController implements Initializable {
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-	System.out.println("Dafuq is this?");
-	for(int i=0;i<10;++i) {
-	    System.out.println("Adding stuff");
-	    test.getChildren().add(new ImageView("http://biking.michael-simons.eu/api/bikingPictures/23.jpg"));
-	}
-    }
-    
-    
-    
+	ObservableList<Node> answer = FXCollections.observableArrayList();
+	
+	final BikingPictureRetrievalService service = new BikingPictureRetrievalService();
+	service.stateProperty().addListener((Observable observable) -> {	
+	    if (service.getState().equals(Worker.State.SUCCEEDED)) {
+		answer.addAll(service.getValue().stream().map(BikingPicture::getSrc).map(ImageView::new).collect(Collectors.toList()));
+	    }
+	});
+	
+	Bindings.bindContentBidirectional(answer, test.getChildren());
+	service.start();	
+    } 
 }
