de1ab5bfa7b6db1a6252d752c054b5faad4f1af9
Michael J. Simons <michael@simons.ac>
2014-10-17 13:18:03 +0200

    Fix title

--- src/main/java/ac/simons/bikingFX/MainApp.java
+++ src/main/java/ac/simons/bikingFX/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("BikingFX");
         stage.setScene(scene);
         stage.show();
     }
