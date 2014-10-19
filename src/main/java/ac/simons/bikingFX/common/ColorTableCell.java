40ab8268b8180a911764beb55de885f21e8c0c6e
Michael J. Simons <michael@simons.ac>
2014-10-19 23:09:09 +0200

    Rename renderer package to common
    Plan to add more than just cell renderer.

--- src/main/java/ac/simons/bikingFX/common/ColorTableCell.java
+++ src/main/java/ac/simons/bikingFX/common/ColorTableCell.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ac.simons.bikingFX.renderer;
+package ac.simons.bikingFX.common;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.TableCell;
--- src/main/java/ac/simons/bikingFX/common/LocalDateTableCell.java
+++ src/main/java/ac/simons/bikingFX/common/LocalDateTableCell.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ac.simons.bikingFX.renderer;
+package ac.simons.bikingFX.common;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
